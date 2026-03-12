d08ca3249bf54811eacd2a48a0e21c8b0aeaa1f7
Erik Mändmaa <erikmandmaa@gmail.com>
2026-03-12 03:32:35 +0200

    Delete Core_Programming_Skills/Programming_Basics directory

--- Core_Programming_Skills/Programming_Basics/2. Write_Correct_Syntax.js
+++
@@ -1,8 +0,0 @@
-const age = input1;
-let canVote;
-if (age >= 18) {
-    canVote = true;
-}   else {
-    canVote = false;
-}
-output = canVote
--- Core_Programming_Skills/Programming_Basics/Your_First_Exercise.js
+++
@@ -1,3 +0,0 @@
-const input1 = 5; 
-const output = input1 * 2; 
-console.log(output); 
